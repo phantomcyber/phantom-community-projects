f18cc30b3d31c1888755e571179d0ef0584f37ea
Sam Hays <7843584+gsamuelhays@users.noreply.github.com>
2020-03-25 14:22:00 -0600

    Automation Games initial commit. (#3)

---
+++ Apps/phautomationgames/autobots_consts.py
@@ -0,0 +1,8 @@
+  
+# File: athena_connector.py
+# Copyright (c) 2017-2019 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+
+# Define your constants here
