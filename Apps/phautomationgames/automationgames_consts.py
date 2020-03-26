ef32f94d2028c8b896f3b330300b9b6921fc8c8a
Sam Hays <7843584+gsamuelhays@users.noreply.github.com>
2020-03-26 10:34:00 -0600

    Change logo. Rename file. (#4)
    * Automation Games initial commit.
    
    * Add logo. Change filename.
    
    Co-authored-by: sam-splunk <51926487+sam-splunk@users.noreply.github.com>

---
+++ Apps/phautomationgames/automationgames_consts.py
@@ -0,0 +1,7 @@
+# File: athena_connector.py
+# Copyright (c) 2017-2019 Splunk Inc.
+#
+# Licensed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+
+
+# Define your constants here
